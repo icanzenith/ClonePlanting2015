de164159666743497e8b21f26953a11b9ccd0d3a
Icanzenith <r.jitpakorn@gmail.com>
2015-10-26 23:20:51 +0700

    Upload GsonTransformer

--- app/src/main/java/org/thailandsbc/cloneplanting/utils/GsonTransformer.java
+++ app/src/main/java/org/thailandsbc/cloneplanting/utils/GsonTransformer.java
@@ -7,13 +7,11 @@ import com.google.gson.Gson;
 public class GsonTransformer implements Transformer {
 
 	public GsonTransformer() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
 	public <T> T transform(String url, Class<T> type, String encoding,
 			byte[] data, AjaxStatus status) {
-		// TODO Auto-generated method stub
 		Gson g = new Gson();
 		return g.fromJson(new String(data), type);
 	}
